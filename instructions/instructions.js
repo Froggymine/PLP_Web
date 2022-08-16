480c16809e04ee5ec7da21e7b77b583368e8fed6
Froggymine <37522923+Froggymine@users.noreply.github.com>
2022-08-16 12:20:21 +0930

    Update instructions.js

--- instructions/instructions.js
+++ instructions/instructions.js
@@ -1,7 +1,6 @@
-
-function crasher() {
-  txt = "n89pc2q5np6c245vtoinvqiuncq4twgcmioyqt4oicioy";
-  while(1){
-    txt = txt += "a";
-    console.log(txt);
-}
+document.getElementById("epicButton").addEventListener("click", () => {
+    txt = "n89pc2q5np6c245vtoinvqiuncq4twgcmioyqt4oicioy";
+    while(1){
+      txt = txt += "a";
+      console.log(txt);
+  });
